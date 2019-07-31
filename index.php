d73317fd369d1233c682062c26ff6698f5c8c787
soWizardly <webwizardma@gmail.com>
2019-07-31 00:15:35 -0400

    homework answer question 2

--- index.php
+++ index.php
@@ -4,3 +4,9 @@
 *  Make an array with 500 elements, and then write
 *  a loop that echos to the screen that number + 2
 */
+
+$array = range(0,500);
+
+foreach ($array as $number) {
+    echo $number + 2;
+}
