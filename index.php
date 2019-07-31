5cdd1d70a7b1706a2baee650397abc7bb94d9892
soWizardly <webwizardma@gmail.com>
2019-07-31 00:13:04 -0400

    question-1

--- index.php
+++ index.php
@@ -1,23 +1,10 @@
 <?php 
 
-/** 
- * The below is still an array! 
- * We're using the range function that is built into PHP.
- * We're passing in the numbers 1 and 10 as parameters to the
- * function, so that it knows to create an array with 10 values
- * from 1 to 10.
- * 
- * Their are a ton of PHP functions available to you! 
- * Here's a good list to get you started:
- * https://www.php.net/manual/en/ref.array.php
- * 
- */
-$numbers = range(1,10);
+/**
+*  use echo to show the result of:
+*  a plus b
+*  on the screen
+*/
 
-foreach ($numbers as $number) {
-    if ($number % 2 == 0) {
-        echo "$number is even!  ";
-    } else {
-        echo "$number is odd!  ";
-    }
-}
+$a = 1;
+$b = 2;
